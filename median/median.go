6cafd506d5a568d1bba36440903c09b13c8c1245
Gergely Brautigam <skarlso777@gmail.com>
2017-06-04 19:36:34 +0200

    45ms

--- median/median.go
+++ median/median.go
@@ -8,20 +8,14 @@ import (
 func findMedianSortedArrays(nums1 []int, nums2 []int) float64 {
 	nums1 = append(nums1, nums2...)
 	sort.Ints(nums1)
-	return median(nums1)
-}
-
-func median(arr []int) (med float64) {
-	l := len(arr)
+	l := len(nums1)
 	if l < 1 {
 		return 0.0
 	}
 	if l&1 == 1 {
-		med = float64(arr[(l-1)/2])
-	} else {
-		med = float64((arr[(l-1)/2] + arr[l/2])) / 2
+		return float64(nums1[(l-1)/2])
 	}
-	return
+	return float64((nums1[(l-1)/2] + nums1[l/2])) / 2
 }
 
 func main() {
