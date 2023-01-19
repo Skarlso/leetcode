e8acdffda89ee4c12cf0b5485cd9c8abdbf04e01
Gergely Brautigam <182850+Skarlso@users.noreply.github.com>
2023-01-19 07:57:34 +0100

    O(1) memory since it is a BINARY SEARCH TREE you idget.

--- find_closes_value_in_bst/main.go
+++ find_closes_value_in_bst/main.go
@@ -9,6 +9,24 @@ type BST struct {
 	Right *BST
 }
 
+// FindClosestValueV2 this one uses O(1) memory.
+func (tree *BST) FindClosestValueV2(target int) int {
+	// Write your code here.
+	min := tree.Value
+	for tree != nil {
+		if abs(min-target) > abs(tree.Value-target) {
+			min = tree.Value
+		}
+
+		if target < tree.Value {
+			tree = tree.Left
+		} else {
+			tree = tree.Right
+		}
+	}
+
+	return min
+}
 func (tree *BST) FindClosestValue(target int) int {
 	// Write your code here.
 	queue := []*BST{tree}
