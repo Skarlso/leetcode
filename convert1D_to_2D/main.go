128fdd762feb28bd48d0d7435a11e28d98e1f2b4
Gergely Brautigam <182850+Skarlso@users.noreply.github.com>
2023-01-27 08:42:34 +0100

    simplified

--- convert1D_to_2D/main.go
+++ convert1D_to_2D/main.go
@@ -1,20 +1,17 @@
 package main
 
 func construct2DArray(original []int, m int, n int) [][]int {
-	var results [][]int
-
 	l := len(original)
 	if m*n != l {
 		return [][]int{}
 	}
 
+	results := make([][]int, m)
+	rows := 0
 	for i := 0; i < m*n; i += n {
 		row := original[i : i+n]
-		results = append(results, row)
-	}
-
-	if len(results) != m {
-		return [][]int{}
+		results[rows] = row
+		rows++
 	}
 	return results
 }
