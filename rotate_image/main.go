70918a17a558d51e13835365ae56536a123db49e
Gergely Brautigam <182850+Skarlso@users.noreply.github.com>
2023-01-31 18:49:57 +0100

    Added rotate image solution

---
+++ rotate_image/main.go
@@ -0,0 +1,21 @@
+package main
+
+// going with the transpose / reverse solution
+func rotate(matrix [][]int) {
+	n := len(matrix)
+
+	for i := 0; i < n; i++ {
+		for j := i + 1; j < n; j++ {
+			matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+		}
+	}
+	for i := 0; i < n; i++ {
+		reverse(matrix[i])
+	}
+}
+
+func reverse(a []int) {
+	for left, right := 0, len(a)-1; left < right; left, right = left+1, right-1 {
+		a[left], a[right] = a[right], a[left]
+	}
+}
